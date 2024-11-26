8c6ccf0f40066f403a59c0e585a14e10d309dc27
Cagatay <cagatay_goz@hotmail.com>
2024-11-27 01:37:04 +0200

    initial commit

---
+++ src/main/java/com/metuncc/society_app_api/SocietyAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.metuncc.society_app_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocietyAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocietyAppApiApplication.class, args);
+	}
+
+}
